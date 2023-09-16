70b3e0bc7368dbcf67c8afdc272d6e69a5aa8216
Arthur Do Nascimento <143120648+oartuu@users.noreply.github.com>
2023-09-16 00:35:17 -0300

    tela login e cadastro

---
+++ script.js
@@ -0,0 +1,25 @@
+function logar() {
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+    if(login == "admin" && senha == "admin"){
+     location.href = "sobre.html";
+    }else{
+     alert('usuario ou senha incorretos');
+    }
+ }
+
+ function cadastro(){
+    location.href="cadastro.html";
+ }
+
+ function cadastrar(){
+   var usuario = document.getElementById('usuario').value;
+   var senha = document.getElementById('senha').value;
+   var confirmarSenha = document.getElementById('confirmar-senha').value;
+   if (senha != confirmarSenha){
+      alert('as senhas não são iguais')
+   }else{
+      alert('cadastrado com sucesso')
+   }
+
+}
